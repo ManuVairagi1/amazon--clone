d694d40725343bd83099e11a9898cc7607634890
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-03-06 22:46:38 +0530

    commits

--- src/Home.js
+++ src/Home.js
@@ -13,23 +13,60 @@ function Home() {
         />
         <div className="home__row">
           <Product
-            title="The lean startup"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
             price={12.8}
-            image="https://cdnb.artstation.com/p/assets/images/images/010/528/903/large/junting-chen-.jpg?1524898887"
-            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/61LAI5UX8vL.jpg"
+            rating={5}
           />
-          <Product />
+          <Product
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
+            price={12.8}
+            image="https://images-na.ssl-images-amazon.com/images/I/51NRPISnBuL._SY291_BO1,204,203,200_QL40_ML2_.jpg"
+            rating={3}
+          />
 
           {/* product */}
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
+            price={12.8}
+            image="https://images-na.ssl-images-amazon.com/images/I/51QTTApN-XL._SX324_BO1,204,203,200_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
+            price={12.8}
+            image="https://cdnb.artstation.com/p/assets/images/images/010/528/903/large/junting-chen-.jpg?1524898887"
+            rating={3}
+          />
+          <Product
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
+            price={12.8}
+            image="https://images-na.ssl-images-amazon.com/images/I/41i9flWTCpL._SX316_BO1,204,203,200_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
           {" "}
-          <Product />
+          <Product
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, quisquam!
+
+"
+            price={12.8}
+            image="https://images-na.ssl-images-amazon.com/images/I/812S0-yEPUL.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
